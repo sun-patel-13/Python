3e01c0b38d79214bdb23ef15259b853de548845d
sun-patel-13 <74000662+sun-patel-13@users.noreply.github.com>
2020-11-05 07:55:35 -0500

    Create Python_bible_problem.py

---
+++ Python_bible_problem.py
@@ -0,0 +1,14 @@
+##Python Bible course excercise, if chapter##
+
+num1=input("Input 1st number: ").strip()
+num2=input("Input 2nd number: ").strip()
+
+print(num1)
+print(num2)
+
+if num1 > num2:
+    print("num1 is greater")
+elif num2>num1:
+    print ("num2 is greater")
+else:
+    print("Both numbers are equal")
